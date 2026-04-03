0323954d42d4ac6d1abcb200ce7e1ec7652d8cde
oriioll <oriolplazas2@gmail.com>
2026-04-04 01:15:39 +0200

    info: add TODO in home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,34 @@ import Navbar from '@/components/Navbar.vue';
 <template>
     <Navbar />
     <h1>HOME VIEW</h1>
+    <pre>
+        PASSOS ENVIAR QUERIES INPUT A HOME
+        1. Event input al search bar
+
+        2. Amb debounce enviar un router push 
+        amb el valor del input, nomes si es mes 
+        de 2 de length
+
+        3. A home, definir un prop amb el que 
+        envia el router
+
+        4. Tindre boolean reactiu per comprovar
+        amb si hi ha una query. Si no hi ha
+        renderitzar populars, si hi ha, render
+        query
+
+        5. Tindre objecte reactiu de la query
+        ja que canvia per event input i fer v-for
+        pel resultat
+    </pre>
 </template>
 
-<style scoped></style>
+<style scoped>
+pre {
+    font-family: 'Onest';
+    font-weight: bold;
+    border: 2px solid var(--silver-main);
+    border-radius: 12px;
+    width: 400px;
+}
+</style>
