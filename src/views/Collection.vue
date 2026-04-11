87ee445e3bd0ef88a02b607776d59aecd3fc433f
oriioll <oriolplazas2@gmail.com>
2026-04-12 00:12:47 +0200

    feat: add collection structure to display correct movies

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -1,20 +1,44 @@
 <script setup lang="ts">
 import CollectionErrorScreen from '@/components/CollectionErrorScreen.vue';
 import { checkIfItsLogged } from '@/services/supabase';
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed, watch } from 'vue'
 import Navbar from '@/components/Navbar.vue';
 import CollectionNavbar from '@/components/CollectionNavbar.vue';
+import { useRoute } from 'vue-router'
 const isLoggedIn = ref(false)
+const route = useRoute()
 
+const section = computed(() => {
+    return route.query.tab as string || 'favoritos'
+})
 onMounted(async () => {
     isLoggedIn.value = await checkIfItsLogged()
+    await loadMovies(section.value)
 })
+
+watch(section, async (newTab) => {
+    await loadMovies(newTab)
+})
+
+const loadMovies = async (section: string) => {
+    switch (section) {
+        case 'favoritos':
+            break;
+        case 'para-ver':
+            break;
+        case 'vistas':
+            break;
+        default:
+            break;
+    }
+}
 </script>
 
 <template>
     <Navbar />
     <main v-if="isLoggedIn">
         <CollectionNavbar />
+
     </main>
     <CollectionErrorScreen v-else />
 </template>
